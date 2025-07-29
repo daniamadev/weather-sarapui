45066a0d9dc46dac2c4ef8da73cca93ed624dfff
daniamadev <danielapamadeu@gmail.com>
2025-07-29 13:38:31 -0300

    removi codigo desnecessario

--- website/script.js
+++ website/script.js
@@ -8,10 +8,3 @@
                 });
             });
         });
-        
-        // Aqui você pode adicionar interações com os gráficos
-        // Por exemplo, tooltips ou filtros interativos
-        console.log('Página carregada com sucesso!');
-        
-        // Exemplo: Carregar gráficos dinâmicos aqui
-        // Você pode usar D3.js, Chart.js ou outra biblioteca de visualização
